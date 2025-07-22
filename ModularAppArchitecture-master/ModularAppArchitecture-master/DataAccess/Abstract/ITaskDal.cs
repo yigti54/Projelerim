1b5d7f1cdf83ba303f2a8d5bcd5854e92b7ec6f6
yigti54 <yigit1903ask@gmail.com>
2025-07-22 16:31:56 +0300

    TaskManager

---
+++ ModularAppArchitecture-master/ModularAppArchitecture-master/DataAccess/Abstract/ITaskDal.cs
@@ -0,0 +1,15 @@
+﻿using Core.DataAccess;
+using Entities.Concrete.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface ITaskDal : IEntityRepository<Task1>
+    {
+
+    }
+}
---
+++ ModularAppArchitecture-master/ModularAppArchitecture-master/WebApi/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Business.Abstract;
+using Core.Utilities.Security.Authorization;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/user")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IAccountService accountService;
+
+    public AccountController(IAccountService accountService)
+    {
+        this.accountService = accountService;
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public IActionResult Login(LoginDto loginDto)
+    {
+        var result = accountService.Login(loginDto);
+        return Ok(result);
+    }
+
+    [AuthorizeRoles("User")]
+    [HttpPost("register")]
+    public IActionResult Register(RegisterDto registerDto)
+    {
+        var result = accountService.Register(registerDto);
+        return Ok(result);
+    }
+}
