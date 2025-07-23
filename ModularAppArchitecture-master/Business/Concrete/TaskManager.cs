32f569d93dd896b4ecc646f9d3dd3989743d61ce
yigti54 <122163321+yigti54@users.noreply.github.com>
2025-07-23 12:04:23 +0300

    Create TaskManager.cs

---
+++ ModularAppArchitecture-master/Business/Concrete/TaskManager.cs
@@ -0,0 +1,46 @@
+using Business.Abstract;
+using Core.Utilities.Result.Abstract;
+using Core.Utilities.Result.Concrete;
+using DataAccess.Abstract;
+using Entities.Concrete.EntityFramework.Entities;
+using Entities.Dtos;
+using System;
+
+namespace Business.Concrete
+{
+    public class TaskManager : ITaskService
+    {
+        private readonly ITaskDal _taskDal;
+        private readonly IErrorLogService _errorLogService;
+
+        public TaskManager(ITaskDal taskDal, IErrorLogService errorLogService)
+        {
+            _taskDal = taskDal;
+            _errorLogService = errorLogService;
+        }
+
+        public IResult AddTask(AddTaskDto addTaskDto, Guid userId)
+        {
+            try
+            {
+                var task = new Task
+                {
+                    UserId = userId,
+                    Title = addTaskDto.Title,
+                    Description = addTaskDto.Description,
+                    DueDate = addTaskDto.DueDate,
+                    IsCompleted = false,
+                    CreatedDate = DateTime.Now
+                };
+
+                _taskDal.Add(task);
+                return new SuccessResult("Görev başarıyla eklendi.");
+            }
+            catch (Exception ex)
+            {
+                _errorLogService.LogError(ex, new { addTaskDto }, userId.ToString(), "AddTask", "TaskManager");
+                return new ErrorResult("Görev eklenirken bir hata oluştu.");
+            }
+        }
+    }
+}
