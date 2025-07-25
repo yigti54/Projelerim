3d87245924cfca4de81c477186d5dc773af1d25d
yigti54 <yigit1903ask@gmail.com>
2025-07-25 17:17:41 +0300

    .

---
+++ TaskManager/TaskManager.DataAccess/ContextDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManager.Entities.Concrete;
+
+namespace TaskManager.DataAccess
+{
+    public class ContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            object value = optionsBuilder.UseSqlServer("Server=intranetdb.tirsan.com;Database=Stajyer;User Id=app_stajyer_user;Password=Asc16rr5asc!;TrustServerCertificate=true");
+        }
+        DbSet<User> Users { get; set; }
+        DbSet<TaskItem> TaskItems {  get; set; }
+    }
+}
